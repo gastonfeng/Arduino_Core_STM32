34d27456ae665b5915104ee20b1a99bce78fc0d8
gastonfeng <gastonfeng@gmail.com>
2019-09-28 11:24:02 +0800

    add stm32f107

---
+++ variants/stm32f107/variant.cpp
@@ -0,0 +1,186 @@
+/*
+  Copyright (c) 2011 Arduino.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include <Arduino.h>
+#include "pins_arduino.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  // Pin number
+  const PinName digitalPin[] = {
+      PA_0,  //D46/A0
+      PA_1,  //D47/A1
+      PA_2,  //D1
+      PA_3,  //D0
+      PA_4,  //D48/A2
+      PA_5,  //D13 - LED
+      PA_6,  //D12
+      PA_7,  //D11
+      PA_8,  //D7
+      PA_9,  //D8
+      PA_10, //D2
+      PA_11, //D37
+      PA_12, //D36
+      PA_13, //D19 - SWD
+      PA_14, //D20 - SWD
+      PA_15, //D21
+      PB_0,  //D49/A3
+      PB_1,  //D41
+      PB_2,  //D40
+      PB_3,  //D3
+      PB_4,  //D5
+      PB_5,  //D4
+      PB_6,  //D10
+      PB_7,  //D22
+      PB_8,  //D15
+      PB_9,  //D14
+      PB_10, //D6
+      PB_11, //D39
+      PB_12, //D38
+      PB_13, //D44
+      PB_14, //D43
+      PB_15, //D42
+      PC_0,  //D51/A5
+      PC_1,  //D50/A4
+      PC_2,  //D28
+      PC_3,  //D29
+      PC_4,  //D45
+      PC_5,  //D35
+      PC_6,  //D34
+      PC_7,  //D9
+      PC_8,  //D33
+      PC_9,  //D32
+      PC_10, //D16
+      PC_11, //D30
+      PC_12, //D17
+      PC_13, //D23
+      PC_14, //D24
+      PC_15, //D25
+      PD_0,  //D26
+      PD_1,  //D27
+      PD_2,  //D31
+      PD_3,  //D26
+      PD_4,  //D27
+      PD_5,  //D31
+      PD_6,  //D26
+      PD_7,  //D27
+      PD_8,  //D31
+      PD_9,  //D26
+      PD_10, //D27
+      PD_11, //D31
+      PD_12, //D26
+      PD_13, //D27
+      PD_14, //D31
+      PD_15,
+      // Duplicated pins in order to be aligned with PinMap_ADC
+      PE_0,
+      PE_1,
+      PE_2,
+      PE_3,
+      PE_4,
+      PE_5,
+      PE_6,
+      PE_11,
+      PE_12,
+      PE_13,
+      PE_14,
+      PE_15};
+
+#ifdef __cplusplus
+}
+#endif
+
+// ----------------------------------------------------------------------------
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  /**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSI)
+  *            SYSCLK(Hz)                     = 64000000
+  *            HCLK(Hz)                       = 64000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 2
+  *            APB2 Prescaler                 = 1
+  *            PLL_Source                     = HSI
+  *            PLL_Mul                        = 16
+  *            Flash Latency(WS)              = 2
+  *            ADC Prescaler                  = 6
+  * @param  None
+  * @retval None
+  */
+  WEAK void SystemClock_Config(void)
+  {
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+    /* Initializes the CPU, AHB and APB busses clocks */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV5;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.Prediv1Source = RCC_PREDIV1_SOURCE_PLL2;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+    RCC_OscInitStruct.PLL2.PLL2State = RCC_PLL2_ON;
+    RCC_OscInitStruct.PLL2.PLL2MUL = RCC_PLL2_MUL8;
+    RCC_OscInitStruct.PLL2.HSEPrediv2Value = RCC_HSE_PREDIV2_DIV5;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    /* Initializes the CPU, AHB and APB busses clocks */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+      Error_Handler();
+    }
+
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+    PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+      Error_Handler();
+    }
+    HAL_RCC_MCOConfig(RCC_MCO, RCC_MCO1SOURCE_HSE, RCC_MCODIV_1);
+    __HAL_RCC_PLLI2S_ENABLE();
+  }
+
+  unsigned int GetRand()
+  {
+    unsigned int seed = millis();
+    return rand_r(&seed) + millis();
+  }
+
+#ifdef __cplusplus
+}
+#endif
---
+++ variants/stm32f107/variant.h
@@ -0,0 +1,193 @@
+/*
+  Copyright (c) 2011 Arduino.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef _VARIANT_ARDUINO_STM32_
+#define _VARIANT_ARDUINO_STM32_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+
+#define HAL_HCD_MODULE_ENABLED 1
+  /*----------------------------------------------------------------------------
+ *        Pins
+ *----------------------------------------------------------------------------*/
+#define PA0 0 // A0
+#define PA1 1 // A1
+#define PA2 2
+#define PA3 3
+#define PA4 4 // A2
+#define PA5 5 // A8 - LED
+#define PA6 6 // A7
+#define PA7 7 // A6
+#define PA8 8
+#define PA9 9
+#define PA10 10
+#define PA11 11
+#define PA12 12
+#define PA13 13 // SWD
+#define PA14 14 // SWD
+#define PA15 15
+
+#define PB0 16 // A3
+#define PB1 17 // A11
+#define PB2 18
+#define PB3 19
+#define PB4 20
+#define PB5 21
+#define PB6 22
+#define PB7 23
+#define PB8 24
+#define PB9 25
+#define PB10 26
+#define PB11 27
+#define PB12 28
+#define PB13 29
+#define PB14 30
+#define PB15 31
+
+#define PC0 32 // A5
+#define PC1 33 // A4
+#define PC2 34 // A9
+#define PC3 35 // A10
+#define PC4 36 // A12
+#define PC5 37 // A13
+#define PC6 38
+#define PC7 39
+#define PC8 40
+#define PC9 41
+#define PC10 42
+#define PC11 43
+#define PC12 44
+#define PC13 45
+#define PC14 46
+#define PC15 47
+
+#define PD0 48
+#define PD1 49
+#define PD2 50
+#define PD3 51
+#define PD4 52
+#define PD5 53
+#define PD6 54
+#define PD7 55
+#define PD8 56
+#define PD9 57
+#define PD10 58
+#define PD11 59
+#define PD12 60
+#define PD13 61
+#define PD14 62
+#define PD15 63
+
+#define PE0 64
+#define PE1 65
+#define PE2 66
+#define PE3 67
+#define PE4 68
+#define PE5 69
+#define PE6 70
+#define PE11 71
+#define PE12 72
+#define PE13 73
+#define PE14 74
+#define PE15 75
+
+#define NUM_DIGITAL_PINS 76
+// This must be a literal with a value less than or equal to to MAX_ANALOG_INPUTS
+#define NUM_ANALOG_INPUTS 14
+#define NUM_ANALOG_FIRST 46
+
+// On-board LED pin number
+#define LED_BUILTIN 13
+#define LED_GREEN LED_BUILTIN
+
+// On-board user button
+#define USER_BTN PC13
+
+// Timer Definitions
+// Do not use timer used by PWM pins when possible. See PinMap_PWM.
+#define TIMER_TONE TIM4
+
+// Do not use basic timer: OC is required
+#define TIMER_SERVO TIM2 //TODO: advanced-control timers don't work
+
+// UART Definitions
+#define SERIAL_UART_INSTANCE 1 //Connected to ST-Link
+// Default pin used for 'Serial' instance (ex: ST-Link)
+// Mandatory for Firmata
+#define PIN_SERIAL_RX PA10
+#define PIN_SERIAL_TX PA9
+
+#define BOARD_USART3_TX_PIN PC10
+#define BOARD_USART3_RX_PIN PC11
+
+#define X1 PD4
+#define X2 PD7
+#define X3 PB6
+#define X4 PB7
+#define X5 PE0
+#define X6 PE1
+#define X7 PE2
+#define X8 PE3
+#define X9 PE4
+#define X10 PE5
+#define X11 PE6
+#define X12 PC13
+#define X13 PC14
+#define X14 PC15
+#define X15 PC0
+#define X16 PC2
+
+#define XA4 PA3
+#define XA7 PA4
+
+
+  unsigned int GetRand();
+  extern const char *hwModel;
+  extern int uptime;
+#ifdef __cplusplus
+} // extern "C"
+#endif
+/*----------------------------------------------------------------------------
+ *        Arduino objects - C++ only
+ *----------------------------------------------------------------------------*/
+
+#ifdef __cplusplus
+// These serial port names are intended to allow libraries and architecture-neutral
+// sketches to automatically default to the correct port name for a particular type
+// of use.  For example, a GPS module would normally connect to SERIAL_PORT_HARDWARE_OPEN,
+// the first hardware serial port whose RX/TX pins are not dedicated to another use.
+//
+// SERIAL_PORT_MONITOR        Port which normally prints to the Arduino Serial Monitor
+//
+// SERIAL_PORT_USBVIRTUAL     Port which is USB virtual serial
+//
+// SERIAL_PORT_LINUXBRIDGE    Port which connects to a Linux system via Bridge library
+//
+// SERIAL_PORT_HARDWARE       Hardware serial port, physical RX & TX pins.
+//
+// SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
+//                            pins are NOT connected to anything by default.
+#define SERIAL_PORT_MONITOR Serial
+#define SERIAL_PORT_HARDWARE Serial
+#endif
+
+#endif /* _VARIANT_ARDUINO_STM32_ */
